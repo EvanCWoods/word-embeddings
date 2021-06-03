dadfeee4126484af6b93d9b87d6aafe1223efb04
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2021-06-03 11:26:00 +1000

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -1,9 +1,11 @@
+# Import libraries
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing.text import one_hot 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Embedding
 
+# Create a sample of reviews
 reviews = ['nice food',
            'amazing restuarant',
            'too good',
@@ -15,6 +17,7 @@ reviews = ['nice food',
            'poor quality',
            'needs improvement']
 
+# Create sentiment labels for the reviews
 sentiment = np.array([1,1,1,1,1,0,0,0,0,0])
 
 VOCAB_SIZE = 30
@@ -24,12 +27,14 @@ EMBEDDING_VECTOR_SIZE = 5
 encoded_reviews = [one_hot(d, VOCAB_SIZE) for d in reviews]
 print(encoded_reviews)
 
+# Pad the reviews with max length length 3
 padded_reviews = tf.keras.preprocessing.sequence.pad_sequences(encoded_reviews,
                                                             maxlen=MAX_LEN,
                                                             padding='post')
 
 print(padded_reviews)
 
+# Create the model
 model = tf.keras.Sequential([
     tf.keras.layers.Embedding(VOCAB_SIZE,
                                 EMBEDDING_VECTOR_SIZE,
@@ -39,6 +44,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
+# Compile the model
 model.compile(loss='binary_crossentropy',
                 optimizer='adam',
                 metrics=['accuracy'])
@@ -48,7 +54,9 @@ y = sentiment
 
 history = model.fit(X, y, epochs=25)
 
+# save the weights of the model's embeddinglayer in the weights variable
 weights = model.get_layer('embedding').get_weights()[0]
 
+# Show the weights for different encoded inputs
 print(weights[0])
 print(weights[1])
